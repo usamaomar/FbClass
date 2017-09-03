4bef137aed0e3af11d7d5c57c5608b552729e176
usamaomar <usamasoftware@yahoo.com>
2017-09-03 23:55:02 +0300

    commit

--- app/src/main/java/adapter/fb/custom/usamaomar/myapplication/ViewClass.java
+++ app/src/main/java/adapter/fb/custom/usamaomar/myapplication/ViewClass.java
@@ -8,12 +8,15 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import java.util.ArrayList;
 
@@ -26,6 +29,7 @@ public class ViewClass extends RelativeLayout {
     private int backGroundSrc;
     private int width;
     private boolean regularStyle;
+    int lastX = 0;
 
     public ViewClass(Context context) {
         super(context);
@@ -36,47 +40,12 @@ public class ViewClass extends RelativeLayout {
         super(context, attributeSet);
         this.context = context;
         init();
-        //getAttrs(attributeSet);
     }
 
-    private void setRangeNumberXml(int rangeNumber) {
-        this.rangeNumber = rangeNumber;
-        init();
-    }
-
-    public void setRangeNumber(int rangeNumber) {
-        this.rangeNumber = rangeNumber;
-        init();
-    }
-
-    public void setbackGroundSrcXml(int backGroundSrc) {
-        this.backGroundSrc = backGroundSrc;
-        init();
-    }
-
-    public void setbackGroundSrc(int backGroundSrc) {
-        this.backGroundSrc = backGroundSrc;
-        init();
-    }
-
-    private void getAttrs(AttributeSet attributeSet) {
-        TypedArray a = context.obtainStyledAttributes(attributeSet, R.styleable.rangeView);
-        String rangeNumber = a.getString(R.styleable.rangeView_range_number);
-        String indicatorSrc = a.getString(R.styleable.rangeView_indicator_src);
-        a.recycle();
-        if (rangeNumber != null) {
-            this.rangeNumber = Integer.parseInt(rangeNumber);
-            setRangeNumberXml(Integer.parseInt(rangeNumber));
-        }
-        if (indicatorSrc != null) {
-            this.backGroundSrc = Integer.parseInt(indicatorSrc);
-            setbackGroundSrcXml(Integer.parseInt(indicatorSrc));
-        }
-    }
 
     private void init() {
         View view = LayoutInflater.from(context).inflate(R.layout.activity_range_view, this);
-        LinearLayout rootLinearLayout = view.findViewById(R.id.rootLinearLayout);
+        LinearLayout mainLinearLayout = (LinearLayout) view.findViewById(R.id.mainLinearLayout);
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(32);
         arr.add(32);
@@ -84,66 +53,39 @@ public class ViewClass extends RelativeLayout {
         arr.add(52);
         arr.add(22);
         arr.add(12);
-        arr.add(32);
-        arr.add(42);
-        arr.add(32);
-        arr.add(22);
-        arr.add(51);
-        arr.add(32);
-        arr.add(32);
-        arr.add(42);
-        arr.add(52);
-        arr.add(22);
-        arr.add(12);
-        arr.add(32);
-        arr.add(42);
-        arr.add(32);
-        arr.add(22);
-        arr.add(51);
-        arr.add(32);
-        arr.add(32);
-        arr.add(42);
-        arr.add(52);
-        arr.add(22);
-        arr.add(12);
-        arr.add(32);
-        arr.add(42);
-        arr.add(32);
-        arr.add(22);
-        arr.add(51);
-        arr.add(32);
-        arr.add(32);
-        arr.add(42);
-        arr.add(52);
-        arr.add(22);
-        arr.add(12);
-        arr.add(32);
-        arr.add(42);
-        arr.add(32);
-        arr.add(22);
-        arr.add(51);
+
         getWid();
 
 
-
-        int mainWidth = width/5;
-        int firstViewWidth = width/3;
-        int mainLobsElevationsNumber = arr.size()/5;
+        int mainWidth = width / 5;
+        int firstViewWidth = width / 3;
+        float mainLobsElevationsNumber = Float.valueOf(arr.size()) / 5;
+        if (mainLobsElevationsNumber % 2 != 0)
+            mainLobsElevationsNumber = (int) mainLobsElevationsNumber + 1;
         for (int y = 0; y < mainLobsElevationsNumber; y++) {
-              LinearLayout linearLayout = new LinearLayout(context);
-            for (int x = 0; x < mainWidth; x++) {
+            LinearLayout linearLayout = new LinearLayout(context);
+            for (int x = lastX; x < arr.size(); x++) {
                 LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(mainWidth, mainWidth);
                 ImageView indicatorButton = new ImageView(context);
+                indicatorButton.setTag(x);
                 indicatorButton.setBackgroundColor(context.getResources().getColor(R.color.colorAccent));
-                layout.setMargins((int) Util.convertDpToPixel(1, context),(int) Util.convertDpToPixel(1, context),(int) Util.convertDpToPixel(1, context),(int) Util.convertDpToPixel(1, context));
+                layout.setMargins((int) Util.convertDpToPixel(1, context), (int) Util.convertDpToPixel(1, context), (int) Util.convertDpToPixel(1, context), (int) Util.convertDpToPixel(1, context));
                 indicatorButton.setLayoutParams(layout);
                 linearLayout.addView(indicatorButton);
+                float xx = indicatorButton.getScaleX();
+                float yy = indicatorButton.getY();
+                indicatorButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.e("s","S");
+                    }
+                });
+                lastX = x;
             }
-            rootLinearLayout.addView(linearLayout);
+            LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(width, mainWidth*2);
+            mainLinearLayout.addView(linearLayout,layout);
         }
     }
-
-
 
 
     private void getWid() {
