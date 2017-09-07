5bb20520cbc694b1f20da90324dbce8e07b6e143
usamaomar <usamasoftware@yahoo.com>
2017-09-07 16:45:27 +0300

    Update ViewClass.java

--- app/src/main/java/adapter/fb/custom/usamaomar/myapplication/ViewClass.java
+++ app/src/main/java/adapter/fb/custom/usamaomar/myapplication/ViewClass.java
@@ -42,7 +42,6 @@ public class ViewClass extends RelativeLayout {
         init();
     }
 
-
     private void init() {
         View view = LayoutInflater.from(context).inflate(R.layout.activity_range_view, this);
         LinearLayout mainLinearLayout = (LinearLayout) view.findViewById(R.id.mainLinearLayout);
@@ -53,45 +52,64 @@ public class ViewClass extends RelativeLayout {
         arr.add(52);
         arr.add(22);
         arr.add(12);
-
+        arr.add(12);
+        arr.add(22);
+        arr.add(12);
+        arr.add(12);
+        arr.add(22);
+        arr.add(12);
+        arr.add(12);
+        arr.add(22);
+        arr.add(12);
+        arr.add(12);
+        arr.add(22);
+        arr.add(12);
+        arr.add(12);
+        arr.add(22);
+        arr.add(12);
+        arr.add(12);
+        arr.add(22);
+        arr.add(12);
+        arr.add(12);
         getWid();
-
-
         int mainWidth = width / 5;
         int firstViewWidth = width / 3;
+        RelativeLayout relativeLayout;
         float mainLobsElevationsNumber = Float.valueOf(arr.size()) / 5;
         if (mainLobsElevationsNumber % 2 != 0)
             mainLobsElevationsNumber = (int) mainLobsElevationsNumber + 1;
         for (int y = 0; y < mainLobsElevationsNumber; y++) {
-            LinearLayout linearLayout = new LinearLayout(context);
-            for (int x = lastX; x < arr.size(); x++) {
+            relativeLayout = new RelativeLayout(context);
+            for (int x = 0; x < 5; x++) {
                 LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(mainWidth, mainWidth);
                 ImageView indicatorButton = new ImageView(context);
-                indicatorButton.setTag(x);
+
                 indicatorButton.setBackgroundColor(context.getResources().getColor(R.color.colorAccent));
                 layout.setMargins((int) Util.convertDpToPixel(1, context), (int) Util.convertDpToPixel(1, context), (int) Util.convertDpToPixel(1, context), (int) Util.convertDpToPixel(1, context));
                 indicatorButton.setLayoutParams(layout);
-                linearLayout.addView(indicatorButton);
-                float xx = indicatorButton.getScaleX();
-                float yy = indicatorButton.getY();
+                relativeLayout.addView(indicatorButton);
+                 if (x % 5 != 0) {// after first view
+                 indicatorButton.setX(((mainWidth + (int) Util.convertDpToPixel(1, context)) * (x % 5)));
+                }
                 indicatorButton.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Log.e("s","S");
+                        Object c = view.getTag();
+                        Log.e("s", "S");
                     }
                 });
-                lastX = x;
+                indicatorButton.setTag(lastX);
+                lastX ++;
+                Log.e("s", "S");
             }
-            LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(width, mainWidth*2);
-            mainLinearLayout.addView(linearLayout,layout);
+            LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(width, mainWidth * 2);
+            mainLinearLayout.addView(relativeLayout, layout);
         }
     }
-
 
     private void getWid() {
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         width = metrics.widthPixels;
     }
-
 }
 
